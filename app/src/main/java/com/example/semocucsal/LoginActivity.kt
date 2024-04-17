54d9775723f7cfb0cd4ec2ccb7748e9e0b087053
Ester Costa de Souza <alwesterc@gmail.com>
2024-04-17 17:59:40 -0300

    tela de login

---
+++ app/src/main/java/com/example/semocucsal/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.semocucsal
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val loginButton: Button = findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, EventListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
